b13699c142631a34aef6df419f8231d6b1c3478c
user <user@localhost.localdomain>
2023-12-02 23:40:14 -0500

    build out php image

---
+++ php/config_files/settings.local.php
@@ -0,0 +1,10 @@
+ $databases['default']['default'] = [
+    'database' => 'drupal',
+    'username' => 'drupal',
+    'password' => 'drupalpassword',
+    'host' => 'drupal-database',
+    'port' => '3306',
+    'driver' => 'mysql',
+    'prefix' => '',
+    'collation' => 'utf8mb4_general_ci',
+ ];
