042682f5732f850f1cdecba660e0fe40a75a0380
user <user@localhost.localdomain>
2023-12-04 13:53:15 -0500

    simplify settings.php and settings local

--- php/config_files/settings.local.php
+++ php/config_files/settings.local.php
@@ -1,10 +1,16 @@
- $databases['default']['default'] = [
-    'database' => 'drupal',
-    'username' => 'drupal',
-    'password' => 'drupalpassword',
-    'host' => 'drupal-database',
-    'port' => '3306',
-    'driver' => 'mysql',
-    'prefix' => '',
-    'collation' => 'utf8mb4_general_ci',
- ];
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupalpassword',
+  'prefix' => '',
+  'host' => 'drupal-database',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+
+$settings['hash_salt'] = 'YOr1oI06r1qckj8oVFxqXFBJ3HTweHCcC2Anmndqt0Dc4kk5v0BdvEBFX8MoWwuef6gAWoD0iQ';
